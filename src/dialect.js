a40416d2586f89e051c56cfa584a305e67629106
fundon <cfddream@gmail.com>
2015-11-20 03:16:19 +0800

    impl select, update

--- src/dialect.js
+++ src/dialect.js
@@ -1,3 +1,63 @@
 
 export default {
+
+  hasTableSql: `
+SELECT count(*)
+  FROM INFORMATION_SCHEMA.tables
+  WHERE table_name = $1
+  AND table_type = 'BASE TABLE'`,
+
+  currentDatabaseSql: 'SELECT CURRENT_DATABASE()',
+
+  select(columns) {
+    const str = columns.join(', ')
+    return str || '*'
+  },
+
+  limit(n) {
+    return n >= 0 ? ` LIMIT ${n}` : ''
+  },
+
+  skip(n) {
+    return n >= 0 ? ` OFFSET ${n}` : ''
+  },
+
+  where(conditions, arr) {
+    var i = arr.length
+    const str = conditions.map(c => {
+      const { column, operator, value } = c
+      arr.push(value)
+      return `${column} ${operator} $${++i}`
+    })
+    return str.length ? ` WHERE ${str}` : ''
+  },
+
+  sort(orders) {
+    const str = orders.map(field => {
+      const [column, flag] = field
+      return `${column} ${flag || ''}`.trimRight()
+    }).join(', ')
+    return str.length ? ` ORDER BY ${str}` : ''
+  },
+
+  group(columns) {
+    const str = columns.join(', ')
+    return str.length ? ` ORDER BY ${str}` : ''
+  },
+
+  returning(columns) {
+    const str = this.select(columns)
+    return str.length ? ` RETURNING ${str}` : ''
+  },
+
+  set(columns, arr) {
+    var i = arr.length
+    const keys = Object.keys(columns)
+    const str = keys.map(k => {
+      arr.push(columns[k])
+      return `${k} = $${++i}`
+    }).join(', ')
+    return str
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@
 import _pg from 'pg'
 import dialect from './dialect'
 
+// https://github.com/brianc/node-pg-native
 const pg = _pg.native || _pg
+// https://github.com/brianc/node-pg-types
+const types = _pg.types
 
 export default {
 
@@ -30,8 +33,10 @@ export default {
   exec(conn, sql = '', values) {
     return conn.then(({ client, done }) => {
       return new Promise((resolve, reject) => {
+        console.log('exec start ==>>')
         console.log('   sql:', sql)
         console.log('values:', values)
+        console.log('exec  end <<==')
         client.query(sql, values, (err, result) => {
           // release pool conn
           done(err)
@@ -47,17 +52,79 @@ export default {
     return dialect
   },
 
-  insert(conn, scope) {
-    return this.exec(conn, '', [])
+  insert(scope) {
+    return this.exec(scope.db.conn, sql, values).then(result => {
+      return result.rows
+    })
   },
 
-  find() {
+  find(scope) {
+    const searcher = scope.searcher
+    const table = searcher._modelName
+    const command = 'SELECT'
+    const columns = searcher._selectionSet
+    const searchConditions = scope.searcher._whereConditions
+    const orders = scope.searcher._sortConditions
+    const groups = scope.searcher._groupConditions
+    const limit = scope.searcher._limit
+    const skip = scope.searcher._skip
+
+    const values = []
+    const sql = `
+${command} ${dialect.select(columns)}
+  FROM ${table}
+  ${dialect.where(searchConditions, values)}
+  ${dialect.sort(orders)}
+  ${dialect.group(groups)}
+  ${dialect.limit(limit)}
+  ${dialect.skip(skip)}`.replace(/\n\s*/g, ' ').trim()
+
+    return this.exec(scope.db.conn, sql, values).then(result => {
+      return result.rows
+    })
   },
 
-  update() {
+  count(scope) {
+    scope.searcher.select('count(*)')
+    return this.find(scope).then(rows => rows[0].count)
   },
 
-  delete() {
+  update(scope) {
+    const searcher = scope.searcher
+    const command = searcher._cmd.toUpperCase()
+    const table = searcher._modelName
+    const columns = searcher._selectionSet
+    const searchConditions = scope.searcher._whereConditions
+    searcher.where('id', scope.value.get('id'))
+    const updateColumns = scope.searcher._updateColumns
+    const values = []
+    const sql = `
+${command} ${table}
+  SET ${dialect.set(updateColumns, values)}
+  ${dialect.where(searchConditions, values)}
+  ${dialect.returning(columns)}`.replace(/\n\s*/g, ' ').trim()
+
+    return this.exec(scope.db.conn, sql, values).then(result => {
+      return result
+    })
+  },
+
+  delete(scope) {
+  },
+
+  hasTable(scope) {
+    const table = scope.search._modelName
+    return this.exec(scope.db.conn, dialect.hasTableSql, [table])
+    .then(result => {
+      return result.rowCount > 0
+    })
+  },
+
+  currentDatabase(scope) {
+    return this.exec(scope.db.conn, dialect.currentDatabaseSql)
+    .then(result => {
+      return result.rows[0]
+    })
   }
 
 }
